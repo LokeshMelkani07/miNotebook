91972f118cebf9b66d6b28f829521f5a41416e1a
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-02-23 14:10:52 +0530

    Delete note functionality has been added successfully

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -51,7 +51,7 @@ router.post(
   }
 );
 
-// Route 3: Update an Existing note using: PUT "/api/notes/updatenote" . Login required
+// Route 3: Update an Existing note using: PUT "/api/notes/updatenote/:id" . Login required
 // We will use PUT request to update the note
 // we need to give id of a note to update a specific note
 // We need only the Autheticated user to update the note
@@ -89,6 +89,37 @@ router.put("/updatenote/:id", fetchuser, async (req, res) => {
       { new: true }
     );
     res.json({ note });
+  } catch (err) {
+    // catch the error if there
+    console.log(err.message);
+    // return server error status code
+    res.status(500).send("Some error occured");
+  }
+});
+
+// Route 4: Deleting an Existing note using: DELETE "/api/notes/deletenote/:id" . Login required
+// We will use DELETE request to update the note
+// we need to give id of a note to delete a specific note
+// We need only the Autheticated user to delete the note
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  try {
+    const { title, description, tag } = req.body;
+    // Check whether the user is original or its someone else
+    // Find the note to be deleted and delete it
+    // req.params.id gives us the id we sent in the endpoint
+    let note = await Note.findById(req.params.id);
+    // If Note with such id does not exists
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    // If user is not original then you cannot delete the note
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+    // Now means Note exists and user is also original so we can give access of updating the note to the user
+
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note has been Deleted", note: note });
   } catch (err) {
     // catch the error if there
     console.log(err.message);
